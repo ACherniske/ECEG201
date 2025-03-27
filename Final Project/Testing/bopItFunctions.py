cd0aceded674785a7d38518a62542cd88652e970
ACherniske <apcherniske@gmail.com>
2025-03-27 14:54:34 -0400

    Backup of Functions

--- Final Project/Testing/bopItFunctions.py
+++ Final Project/Testing/bopItFunctions.py
@@ -1,3 +1,13 @@
+from digitalio import DigitalInOut, Direction, Pull
+import board
+from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
+
+Hall = DigitalInOut(board.D16)
+Hall.direction = Direction.INPUT
+kit = MotorKit()
+analog_in = analogio.AnalogIn(board.A1)
+
 def get_voltage(pin): # Specify Analog Pin
     return (pin.value * 3.3) / 65536  # Convert 16-bit analog reading to voltage
 
@@ -9,7 +19,8 @@ def home_Motor():
     #neoFun.set_ring_color((0,255,0)) Commented out Implementation
     kit.stepper1.release()
 
-def read_3_buttons(analog_read_function, thresholds=(1.0, 1.5, 2.0)): # (0,1,2)
+def detect_Button(analog_read_function, thresholds=(1.0, 1.5, 2.0)): # (0,1,2)
+    # ex. detect_Button(get_voltage(analog_in), (1.0, 1.5, 2.0))
     voltage = analog_read_function()
     button_pressed = None  # Initialize button_pressed to None
 
--- Final Project/Testing/code.py
+++ Final Project/Testing/code.py
@@ -10,26 +10,8 @@ from digitalio import DigitalInOut, Direction, Pull
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 import neoPixelFunctions as neoFun
-
-Hall = DigitalInOut(board.D16)
-Hall.direction = Direction.INPUT
-kit = MotorKit()
-
-"""while True:
-    if Hall.value:
-        neoFun.set_ring_color((255,0,0))
-        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-    else:
-        neoFun.set_ring_color((0,255,0))
-        kit.stepper1.release()
-"""
-def home_Motor():
-    while Hall.value:
-        neoFun.set_ring_color((255,0,0))
-        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-
-    neoFun.set_ring_color((0,255,0))
-    kit.stepper1.release()
+import bopItFunctions as bopIt
 
 while True:
-    home_Motor()
+    bopIt.home_Motor()
+    time.sleep(0.1)
